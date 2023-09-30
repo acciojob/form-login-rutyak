4cdcdd2a3bfd680733587a43c6ec9b5166811158
rutyak <132219103+rutyak@users.noreply.github.com>
2023-09-30 13:50:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+    let fname = document.getElementById("firstname");
+    let lname = document.getElementById("lastname");
 
+    alert(fname.value +" "+lname.value);
 }
+
